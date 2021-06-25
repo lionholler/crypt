e324cb522004fc56879bba5d800e8e21484352ee
lionholler <82542096+lionholler@users.noreply.github.com>
2021-06-25 12:39:37 +0200

    header file

---
+++ chacha/h/cha.h
@@ -0,0 +1,41 @@
+
+#define min(a, b) (a > b) ? a : b
+#define max(a, b) (a > b) ? a : b
+
+static void populate(uint32_t *c) {
+	for (int i = 0; i < 16; i++) {
+		*c++ = 0x00;
+	}
+}
+
+static int hextoint(char c) {
+	return (c < 58) ? c - '0' : c - 87;
+}
+
+static unsigned long long pow(const int b, const int n) {
+	unsigned long long p = 1;
+	for (int i = 0; i < n; i++) {
+		p *= b;
+	}
+	return p;
+}
+
+void parsehex(char *s, uint32_t *c) {
+	populate(c);
+	for (int j = 0; j < 16; s+=8, j++) {
+		for (int i = 0; i < 8; i++) {
+			c[j] += hextoint(s[i]) * pow(16, 7 - i);
+		}
+	}
+}
+
+/* flag manipulations */
+uint8_t activate_bit(uint8_t b, short n) {
+// activate a bit at the nth position
+	return b | (1 << n);
+}	
+
+int check_bit_mask(uint8_t b, short n) {
+// check if bit at nth position is active
+	return b & (1 << n);
+}
